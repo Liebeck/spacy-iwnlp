e359fa9c9d31652115c6da6762ade260ceddee26
Matthias Liebeck <Liebeck@users.noreply.github.com>
2018-04-24 15:58:25 +0200

    Prepare PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='spacy-iwnlp',
+    packages=['spacy-iwnlp'],
+    version='0.0.1',
+    description='Integration of IWNLP-py as spaCy extension',
+    author='Matthias Liebeck',
+    author_email='liebeck@cs.uni-duesseldorf.de',
+    url='https://github.com/Liebeck/space-iwnlp',
+    download_url='https://github.com/Liebeck/spacy-iwnlp/archive/0.0.1.tar.gz',
+    keywords=['IWNLP', 'NLP', 'German', 'lemmatization', 'Wiktionary', 'spaCy'],
+    classifiers=[],
+)
