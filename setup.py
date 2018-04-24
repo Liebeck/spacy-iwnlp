c3208512bdcce8fb892a1fe0c702a2a53b62d7bf
Matthias Liebeck <Liebeck@users.noreply.github.com>
2018-04-24 16:59:05 +0200

    Increase version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='spacy_iwnlp',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='Integration of IWNLP-py as spaCy extension',
     author='Matthias Liebeck',
     author_email='liebeck@cs.uni-duesseldorf.de',
     url='https://github.com/Liebeck/spacy-iwnlp',
-    download_url='https://github.com/Liebeck/spacy-iwnlp/archive/0.0.1.tar.gz',
+    download_url='https://github.com/Liebeck/spacy-iwnlp/archive/0.0.2.tar.gz',
     keywords=['IWNLP', 'NLP', 'German', 'lemmatization', 'Wiktionary', 'spaCy'],
     install_requires=['spacy>=2.0.0,<3.0.0',
                       'iwnlp>=0.1.7']
